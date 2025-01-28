ad2449595816cdab7cea6d5d6f023515be04afa6
Elcand <tjandraraharja99@gmail.com>
2025-01-28 15:05:24 +0700

    feat: done connect church for admin to user

--- resources/views/church.blade.php
+++ resources/views/church.blade.php
@@ -4,54 +4,37 @@
             <h3 class="display-4 mb-9 text-uppercase font-bold">List Gereja
             </h3>
         </div>
-        {{-- <div class="swiper-container blog grid-view mb-10" data-margin="30" data-dots="true" data-items-xl="3"
+        <div class="swiper-container blog grid-view mb-10" data-margin="30" data-dots="true" data-items-xl="3"
             data-items-md="2" data-items-xs="1">
             <div class="swiper">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide">s
-                        @forelse ($churches as $church)
+                    @forelse ($churches as $church)
+                        <div class="swiper-slide">
                             <article>
-                                <figure class="overlay overlay-1 hover-scale rounded mb-6"><a
-                                        href="{{ route('church.show', $church->id) }}   "> <img
+                                <figure class="overlay overlay-1 hover-scale rounded"
+                                    style="max-height: 400px; width: auto;"><a href="#"> <img
                                             src="{{ asset('storage/' . $church->image) }}" alt="" /></a>
                                     <figcaption>
                                         <h5 class="from-top mb-0">Read More</h5>
                                     </figcaption>
                                 </figure>
                                 <div class="post-header">
-                                    <h2 class="post-title h3 mb-3"><a class="link-dark"
+                                    <h2 class="post-title h3 mb-2 mt-4"><a class="link-dark"
                                             href="#">{{ $church->name }}</a>
                                     </h2>
+                                    <p><a class="link-dark" href="#">{{ $church->address }}</a>
+                                    </p>
                                 </div>
                             </article>
-                        @empty
-                            <tr>
-                                <td colspan="3" class="text-center">Data Gereja belum Tersedia.</td>
-                            </tr>
-                        @endforelse
-                    </div>
+                        </div>
+                    @empty
+                        <tr>
+                            <td colspan="3" class="text-center">Data Gereja belum Tersedia.</td>
+                        </tr>
+                    @endforelse
+
                 </div>
             </div>
         </div>
-
-
-        <div class="card border-0 shadow-sm rounded">
-            <div class="card-body">
-                @forelse ($churches as $church)
-                    <tr>
-                        <td>{{ $church->name }}</td>
-                        <td>{{ $church->pastor }}</td>
-                        <td>{{ $church->address }}</td>
-                        <td>{{ $church->city }}</td>
-                        <td>
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="3" class="text-center">Data Gereja belum Tersedia.</td>
-                    </tr>
-                @endforelse
-            </div>
-        </div> --}}
     </div>
 </div>
